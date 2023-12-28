95add820771598cb87940282c48eacc68d9d33c9
nathandinh <nathan.dinh03@outlook.com>
2023-12-28 04:32:06 -0500

    Added a vercel json

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,43 @@
 <script setup>
-import Header from './components/page_components/Header.vue'
-import Footer from './components/page_components/Footer.vue'
-import SubNavigation from './components/page_components/SubNavigation.vue';
+import Header from "./components/page_components/Header.vue";
+import Footer from "./components/page_components/Footer.vue";
+import SubNavigation from "./components/page_components/SubNavigation.vue";
 
-import { RouterView  } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-      <Header/>
-      <section class="main-content-container">
-         <div id="content-one">
-            <SubNavigation/>
-         </div>
-         <div id="content-two">
-            <RouterView/>
-         </div>
-      </section>   
-      <Footer/>
+  <Header />
+  <section class="main-content-container">
+    <div id="content-one">
+      <SubNavigation />
+    </div>
+    <div id="content-two">
+      <RouterView />
+    </div>
+  </section>
+  <Footer />
 </template>
 
 <style scoped>
-   .main-content-container{
-      display: flex;
-      padding: 2em;
-   }
+.main-content-container {
+  display: flex;
+  padding: 2em;
+}
 
-   #content-one{
-      width: 100%; 
-      max-width: 15%;
-      max-height: 80vh;
-      height: 80vh; 
-      padding: 1em;
-      background-color: rgb(34, 34, 34);
-      color: lightgray; 
-      border-radius: 5px;
-   }
+#content-one {
+  width: 100%;
+  max-width: 15%;
+  max-height: 80vh;
+  height: 80vh;
+  padding: 1em;
+  background-color: rgb(34, 34, 34);
+  color: lightgray;
+  border-radius: 5px;
+}
 
-   #content-two{
-      max-width: 85%;
-      width: 100%;
-   }
+#content-two {
+  max-width: 85%;
+  width: 100%;
+}
 </style>
-
