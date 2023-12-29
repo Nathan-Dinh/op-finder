21506c06df273a1dcbbd284c48f4603abd60986d
nathandinh <nathan.dinh03@outlook.com>
2023-12-28 23:31:52 -0500

    Fixed spelling mistake in file name

---
+++ src/components/operatorPortraitCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {defineProps} from "vue"
+const props = defineProps(["opImage"])
+console.log(props)
+</script>
+
+<template>
+  <div class="portrait-container">
+    <img :src="props.opImage" class="image-size" alt="#">
+  </div>
+</template>
+
+<style scoped>
+.portrait-container{
+  display: flex;
+  justify-content: center;
+  padding: 1em;
+}
+.image-size{
+  width: 100%;
+  height: auto;
+}
+</style>
