3328fe80c7eab81fc959b132537beefa4fcbf918
nathandinh <nathan.dinh03@outlook.com>
2023-12-28 20:46:19 -0500

    Replaced css style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,13 @@
 <script setup></script>
 
 <template>
-  <div id="container">
-     <h1>Home view</h1>
+  <div class="container">
+    <div>
+      <h1 class="title">Home view</h1>
+    </div>
   </div>
 </template>
 
 <style scoped>
-#container {
-  padding:0 1em;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-}
+
 </style>
