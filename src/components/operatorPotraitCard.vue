34558a76d6d89bc17ad695f8c8911eef08d4a325
nathandinh <nathan.dinh03@outlook.com>
2023-12-28 20:45:43 -0500

    Implemented portrait template

---
+++ src/components/operatorPotraitCard.vue
@@ -0,0 +1,16 @@
+<script scoped></script>
+
+<template>
+  <div class="portrait-container">
+    <h1>hello</h1>
+  </div>
+</template>
+
+<style scoped>
+.portrait-container{
+  display: flex;
+  justify-content: center;
+  background-color: aqua;
+  padding: 1em;
+}
+</style>
