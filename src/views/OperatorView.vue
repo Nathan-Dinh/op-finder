da6d504e1622e11e433642ff14a18cfd029e70b1
nathandinh <nathan.dinh03@outlook.com>
2024-01-04 19:01:20 -0500

    Created a operator information component

---
+++ src/views/OperatorView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import operatorTemplate from "@/components/operatorTemplate.vue";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+
+let opInfo = ref(null)
+
+const operator = async (op) => {
+  const response = await fetch(`http://localhost:3000/operators/${op}`);
+  const data = await response.json();
+  console.log(data)
+  opInfo.value = data
+};
+
+operator(route.params.operator);
+</script>
+
+<template>
+    <div v-for="info in opInfo">
+        <operatorTemplate :opInfo="info" />
+    </div>
+</template>
+
+<style></style>
