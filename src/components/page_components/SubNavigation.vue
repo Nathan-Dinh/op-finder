d033b4fd402fac7a10081628f8689ba9679ed826
nathandinh <nathan.dinh03@outlook.com>
2024-01-04 19:12:50 -0500

    Added a animation to button click

--- src/components/page_components/SubNavigation.vue
+++ src/components/page_components/SubNavigation.vue
@@ -1,11 +1,25 @@
 <script setup>
 import { ref } from "vue";
 import { RouterLink } from "vue-router";
+import { useCurrentTab } from "../../store/useCurrentTab";
+
+const test = ref(useCurrentTab((state) => state.currentOptionTab));
+const { updateOptionTab } = useCurrentTab();
 
 const sideNavItems = ref([
-  { name: "", path: "/", icon: "md-home-outlined" , tooltip: "Home"},
-  { name: "", path: "/search", icon: "md-personsearch-twotone", tooltip: "Search"},
-  { name: "", path: "/gallery", icon: "md-viewlist-outlined", tooltip: "Gallery"},
+  { name: "Home", path: "/", icon: "md-home-outlined", tooltip: "Home" },
+  {
+    name: "Search",
+    path: "/search",
+    icon: "md-personsearch-twotone",
+    tooltip: "Search",
+  },
+  {
+    name: "Gallery",
+    path: "/gallery",
+    icon: "md-viewlist-outlined",
+    tooltip: "Gallery",
+  },
 ]);
 </script>
 
@@ -14,21 +28,20 @@ const sideNavItems = ref([
     <div>
       <img src="/Rhodes_Island.webp" alt="icon" class="icon" />
     </div>
-    <div class="title-name">
-      <h1></h1>
-    </div>
   </div>
   <hr />
   <nav class="nav-container">
-      <router-link
-        class="side-nav-links tooltip"
-        :to="items.path"
-        v-for="items in sideNavItems"
-      >
+    <router-link
+      @click="updateOptionTab(items.name)"
+      class="pushable tooltip"
+      :to="items.path"
+      v-for="items in sideNavItems"
+    >
+      <div :class="test === items.name ? 'front highlighted' : 'front'">
         <v-icon :name="items.icon" scale="2" />
-        {{ items.name }}
-        <span class="tooltiptext">{{items.tooltip}}</span>
-      </router-link>
+        <span class="tooltiptext">{{ items.tooltip }}</span>
+      </div>
+    </router-link>
   </nav>
 </template>
 
@@ -42,19 +55,43 @@ const sideNavItems = ref([
   padding: 1em;
   font-weight: bold;
 }
+.highlighted {
+  color: white !important;
+  background-color: rgb(6, 60, 92) !important;
+}
 .nav-container {
   display: flex;
   flex-direction: column;
-  font-size: 1.5em;
+  align-items: center;
   font-weight: lighter;
 }
-.side-nav-links {
-  padding: 0.7em 1em;
-  margin: 0.2em 0;
-  border-radius: 5px;
-  color: lightgray;
-}
+
 .side-nav-links:hover {
-  background-color: rgb(80, 79, 79);
+  color: white;
+}
+.pushable {
+  color: rgba(255, 255, 255, 0.87);
+  background: hsl(212, 31%, 11%);
+  border-radius: 12px;
+  margin: 1em 0;
+  cursor: pointer;
+}
+
+.front {
+  display: block;
+  padding: 1em;
+  border-radius: 12px;
+  background: #192535;
+  border: 0.5px solid #25354b;
+  color: rgb(119, 116, 116);
+  transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
+}
+.pushable:hover .front {
+  transform: translateY(-6px);
+  transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+}
+
+.pushable:active .front {
+  transform: translateY(-2px);
 }
 </style>
