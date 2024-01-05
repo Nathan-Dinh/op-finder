5f00c1397cc52395986ce45860ee04ea263cad42
nathandinh <nathan.dinh03@outlook.com>
2024-01-04 19:03:17 -0500

    Reworked how the overall information is displayed

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,35 +1,44 @@
 <script setup>
-import OperatorPortraitCard from "../components/operatorPortraitCard.vue";
 import { ref } from "vue";
+import OperatorPortraitCard from "../components/operatorPortraitCard.vue";
+import { useCurrentTab } from "../store/useCurrentTab.js";
+import {Capitalize} from '../utilities/formatting.js'
+
+const update = useCurrentTab((state) => state.updateOptionTab);
 let listOfOp = ref(null);
 
 const FetchOperators = async () => {
   const response = await fetch(
-    "https://arknights-api-nathan-dinh.onrender.com/operators"
+    "http://localhost:3000/operators"
   );
   const data = await response.json();
   listOfOp.value = data;
 };
+
+update("Gallery");
 FetchOperators();
 </script>
 
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">Gallery view</h1>
-    </div>
     <div class="main-container">
-      <div v-for="op in listOfOp">
-        <OperatorPortraitCard :opImage="op.imgUrl" />
-      </div>
+        <div v-for="item in listOfOp">
+          <div>
+            <h1>{{Capitalize(item.name)}}</h1>
+          </div>
+          <OperatorPortraitCard :op="item" />
+        </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.main-container {
-  display: grid;
-  grid-template-columns: repeat(3, auto);
-  grid-gap: 1em;
+.main-container{
+  overflow: auto;
+  min-height: 80vh;
+  max-height: 80vh;
+  padding: 1em;
+  border-left: 1px solid ;
+  text-align: center;
 }
 </style>
