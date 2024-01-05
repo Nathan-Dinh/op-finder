e7f107462a3c62395db3cc240920cbefc38124f8
nathandinh <nathan.dinh03@outlook.com>
2024-01-04 19:15:55 -0500

    Reworked css

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,8 @@
 <script setup>
   import { ref } from 'vue'
+  import Button from 'primevue/button';
+  import InputText from 'primevue/inputtext';
+
   const op = ref('')
 </script>
 
@@ -8,10 +11,10 @@
     <form @submit.prevent  method="get" class="search-box">
       <div class="user-box">
         <label for="operatorName"></label>
-        <input v-model="op" type="text" name="operatorName" />
+        <InputText v-model="op" type="text" name="operatorName" />
       </div>
       <div>
-        <button @click="$emit('op-name-tracker', op)" class="button-31">search</button>
+        <Button @click="$emit('event-click-handler', op)" class="button-31" label="search"/>
       </div>
     </form>
   </div>
@@ -23,30 +26,25 @@
   align-items: center;
 }
 .search-box .user-box input {
-  margin: 0.8em;
+  margin: 1em;
   width: 100%;
-  font-size: 42px;
+  font-size: 32px;
   color: #fff;
-  border: none;
-  border-bottom: 1px solid #fff;
-  outline: none;
-  background: transparent;
 }
 .button-31 {
-  background-color: #222;
+
   border-radius: 5px;
-  border-style: none;
   box-sizing: border-box;
   color: #fff;
   display: inline-block;
-  font-size: 24px;
+  font-size: 1.2em;
   line-height: 1.5;
   margin: 0 2em;
   cursor: pointer;
-  padding: 9px 20px 8px;
+  padding: 5px 20px 5px;
 }
 .button-31:hover,
 .button-31:focus {
-  opacity: .75;
+  opacity: .75  ;
 }
 </style>
