e41f2f71e2c290b50cadd15d5cd641d88b7a5d4d
nathandinh <nathan.dinh03@outlook.com>
2024-01-04 19:14:16 -0500

    Created a select box

---
+++ src/components/SelectBox.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref,defineProps } from "vue";
+import Dropdown from "primevue/dropdown";
+const selectedRoute = ref("Name");
+const props = defineProps(["selectedRoute"])
+
+const OPTION_LIST_ITEM = [
+  { value: "Name" },
+  { value: "Position" },
+  { value: "Profession" },
+];
+</script>
+
+<template>
+  <div>
+    <Dropdown
+      v-model="selectedRoute"
+      :options="OPTION_LIST_ITEM"
+      optionLabel="value"
+      class="dropdown-menu"
+    />
+  </div>
+</template>
+
+<style scoped>
+.dropdown-menu {
+  width: 10em;
+}
+</style>
