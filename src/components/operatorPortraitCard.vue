a08cbf12c73fd565f50584b254c3e49cda59490a
nathandinh <nathan.dinh03@outlook.com>
2024-01-06 00:01:56 -0500

    Changed the width size of the image

--- src/components/operatorPortraitCard.vue
+++ src/components/operatorPortraitCard.vue
@@ -1,25 +1,24 @@
 <script setup>
-import {defineProps} from "vue"
-const props = defineProps(["op"])
-
+import { defineProps } from "vue";
+const props = defineProps(["op"]);
 </script>
 
 <template>
   <div class="portrait-container">
-    <img :src="props.op.imgUrl" class="image-size" alt="#">
+    <img :src="props.op.imgUrl" class="image-size" alt="#" />
   </div>
 </template>
 
 <style scoped>
-.portrait-container{
+.portrait-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   margin-bottom: 2em;
 }
-.image-size{
-  width: 50%;
+.image-size {
+  width: 55%;
   height: auto;
 }
 </style>
