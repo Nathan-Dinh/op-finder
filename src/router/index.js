989d39cd860fa640822b8567a065dfd84282e9a9
nathandinh <nathan.dinh03@outlook.com>
2024-01-06 00:08:05 -0500

    Implemented unplugin-vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import SearchView from "../views/SearchView.vue";
-import GalleryView from "../views/GalleryView.vue";
-import OperatorView from "../views/OperatorView.vue";
+import { createRouter, createWebHistory } from "vue-router/auto";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/search",
-      name: "search",
-      component: SearchView,
-    },
-    {
-      path: "/gallery",
-      name: "gallery",
-      component: GalleryView,
-    },
-    {
-      path: "/:operator",
-      name: "operator",
-      component: OperatorView,
-    },
-  ],
+  history: createWebHistory(),
 });
 
 export default router;
