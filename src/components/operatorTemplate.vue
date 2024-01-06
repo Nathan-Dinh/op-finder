d8d2f14c8628ecdcbca45acf8e4ff297ec87b0e9
nathandinh <nathan.dinh03@outlook.com>
2024-01-06 00:02:53 -0500

    Improved html tag readability and removed unnecessary code

--- src/components/operatorTemplate.vue
+++ src/components/operatorTemplate.vue
@@ -1,20 +1,7 @@
 <script setup>
 const props = defineProps(["opInfo"]);
 import Card from "primevue/card";
-
-const CapitalizeName = (str) => {
-  let fullName = "";
-  const STR_ARRAY = str.split(" ");
-  STR_ARRAY.forEach((str) => {
-    fullName +=
-      str.charAt(0).toUpperCase() + str.substring(1, str.length) + " ";
-  });
-  return fullName;
-};
-
-if (props.opInfo != null) {
-  console.log(CapitalizeName(props.opInfo.name));
-}
+import { Capitalize } from "@/utilities/formatting";
 </script>
 
 <template>
@@ -22,13 +9,15 @@ if (props.opInfo != null) {
     <template #content>
       <div class="card-container" v-if="props.opInfo != null">
         <div class="operator-image">
-          <img class="image-resize" :src="props.opInfo.imgUrl" alt="opImage" />
+          <img
+            class="image-resize"
+            :src="props.opInfo.imgUrl"
+            alt="Operator Image"
+          />
         </div>
-        <div>
+        <section>
           <div class="operator-name">
-            <h1>
-              {{ CapitalizeName(props.opInfo.name).trim() }}
-            </h1>
+            <h1>{{ Capitalize(props.opInfo.name) }}</h1>
           </div>
           <div class="requirement-sub-header">
             <h2 class="sub-header">Requirement tag</h2>
@@ -50,7 +39,7 @@ if (props.opInfo != null) {
           <div class="profile">
             <p class="sub-content">{{ props.opInfo.profile }}</p>
           </div>
-        </div>
+        </section>
       </div>
     </template>
   </Card>
@@ -67,6 +56,8 @@ if (props.opInfo != null) {
   grid-area: profile-sub;
 }
 .operator-image {
+  display: flex;
+  justify-content: center;
   grid-area: image;
 }
 .operator-name {
@@ -93,31 +84,44 @@ if (props.opInfo != null) {
 .sub-header {
   display: flex;
   align-items: center;
-  font-size: 1.5em;
+  font-size: 1.2em;
   padding: 0.5em;
   border-radius: 5px;
   background: rgb(129, 129, 129);
 }
 .sub-content {
-  font-size: 1.2em;
+  font-size: 1em;
   padding: 1em;
 }
 .image-resize {
-  max-width: 100%;
+  max-width: 42em;
   height: auto;
 }
 .card-container {
   display: grid;
   grid-template-areas:
-    "image name name"
-    "image requirement-sub requirement-sub"
-    "image tags tags"
-    "image arch-sub arch-sub"
-    "image arch arch"
-    "image profile-sub profile-sub"
-    "image profile profile";
+    "image name name name name"
+    "image requirement-sub requirement-sub requirement-sub requirement-sub"
+    "image tags tags tags tags"
+    "image arch-sub arch-sub arch-sub arch-sub"
+    "image arch arch arch arch"
+    "image profile-sub profile-sub profile-sub profile-sub"
+    "image profile profile profile profile";
   border-radius: 5px;
+  align-items: center;
   height: 80vh;
   padding: 2em;
+}
+
+@media only screen and (max-width: 1200px) {
+  .card-container {
+    display: flex;
+    flex-direction: column;
+    height: auto;
+  }
+  .image-resize {
+    width: 60%;
+    height: auto;
+  }
 }
 </style>
