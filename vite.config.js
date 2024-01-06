96221eb7d41a24299e9163782532aa6377b843e4
nathandinh <nathan.dinh03@outlook.com>
2024-01-05 23:56:07 -0500

    Added unplugin to config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import VueRouter from 'unplugin-vue-router/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    VueRouter({
+      routesFolder: 'src/pages',
+    }),
+    vue()
   ],
   resolve: {
     alias: {
