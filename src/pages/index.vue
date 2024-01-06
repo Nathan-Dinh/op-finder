52fe0d8f9b6d0b27a6da55f6e5175e6741ca26b2
nathandinh <nathan.dinh03@outlook.com>
2024-01-06 00:06:44 -0500

    Renamed component

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {onBeforeMount} from "vue"
+import { useCurrentTab } from "../store/useCurrentTab.js";
+
+onBeforeMount(() => {
+  const update = useCurrentTab(state => state.updateOptionTab)
+  update("Home")
+});
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1 class="title">Home view</h1>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
