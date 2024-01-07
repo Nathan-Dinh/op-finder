5c19ccaa2aae779edc4b963dcfc5e39738ee104b
nathandinh <nathan.dinh03@outlook.com>
2024-01-07 03:43:36 -0500

    Created a tree select

---
+++ src/components/TreeSelect.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref,defineProps } from "vue";
+import Dropdown from 'primevue/dropdown';
+import { useCurrentTab } from "@/store/useCurrentTab";
+
+const currentTab = ref("")
+const props = defineProps(['items'])
+</script>
+
+<template>
+    <div class="container">
+        <Dropdown @change="$emit('change-handler',currentTab.label)" v-model="currentTab" :options="props.items" optionLabel="label" placeholder="Name" class="stretch-select"/>
+    </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+.stretch-select {
+  width: 100%;
+}</style>
