939a6f0e561ac5accd95a6009690f6e2ffd418e3
nathandinh <nathan.dinh03@outlook.com>
2024-01-12 01:22:09 -0500

    Refactored to code implementing new template component

--- src/components/api_documentation/ResponseTemplate.vue
+++ src/components/api_documentation/ResponseTemplate.vue
@@ -1,47 +1,31 @@
 <script setup>
 import { defineProps } from "vue";
+import PropWithoutCode from "./PropWithoutCode.vue";
 import Panel from "primevue/panel";
 const props = defineProps(["items"]);
+
+const OPERATOR_PROP = {
+  name: "operators",
+  type: "Array of operators",
+  description: ""
+}
 </script>
 
 <template>
   <Panel toggleable class="sub-container">
     <template #header>
       <div class="header-container">
-        <div class="prop-container">
-          <div>
-            <h4>operators</h4>
-          </div>
-          <div>
-            <p>Array of operators</p>
-          </div>
-        </div>
+        <PropWithoutCode :item="OPERATOR_PROP"/>
       </div>
     </template>
-    <div v-for="item in props.items" class="prop-container">
-      <div>
-        <h4>{{ item.name }}</h4>
-        <span>{{ item.type }} </span>
-      </div>
-      <p>{{ item.description }}</p>
-    </div>
+    <PropWithoutCode
+      v-for="item in props.items"
+      :item="item"
+    />
   </Panel>
 </template>
 
 <style setup>
-.prop-container {
-  padding: 1em;
-}
-.prop-container div {
-  display: flex;
-  align-items: center;
-}
-.prop-container div h4 {
-  padding: 0.2em 0.5em 0.2em 0.5em;
-  background: rgb(142, 156, 165);
-  border-radius: 5px;
-  margin-right: 0.5em;
-}
 .sub-container .header-container {
   display: flex;
 }
