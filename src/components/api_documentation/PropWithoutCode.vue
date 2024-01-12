8f20c6c951ae00c6ae5c2d3b897669b7e924bca6
nathandinh <nathan.dinh03@outlook.com>
2024-01-12 01:21:32 -0500

    Added new template component

---
+++ src/components/api_documentation/PropWithoutCode.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps(["item"]);
+</script>
+
+<template>
+  <div class="prop-container">
+    <div>
+      <h4>{{ props.item.name }}</h4>
+      <span>{{ props.item.type }}</span>
+    </div>
+    <p>{{ props.item.description }}</p>
+  </div>
+</template>
+
+<style scoped>
+.prop-container {
+  padding: 1em;
+}
+.prop-container div {
+  display: flex;
+  align-items: center;
+}
+.prop-container div h4 {
+  padding: 0.2em 0.5em 0.2em 0.5em;
+  background: rgb(142, 156, 165);
+  border-radius: 5px;
+  margin-right: 0.5em;
+}
+</style>
