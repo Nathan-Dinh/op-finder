1c04f38e8c2e9f6562b6af5b28b0a99355f16e46
nathandinh <nathan.dinh03@outlook.com>
2024-01-13 23:48:18 -0500

    Added header for operator name and set router link with template

--- src/components/operatorPortraitCard.vue
+++ src/components/operatorPortraitCard.vue
@@ -1,12 +1,25 @@
 <script setup>
+import { Capitalize } from "../utilities/formatting.js";
 import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
 const props = defineProps(["op"]);
 </script>
 
 <template>
+  <RouterLink :to="props.op.name">
   <div class="portrait-container">
-    <img :src="props.op.imgUrl" class="image-size" alt="#" />
+    <header class="header">
+      <h1>{{ Capitalize(props.op.name) }}</h1>
+    </header>
+    <img
+      role="presentation"
+      loading="lazy"
+      :src="props.op.imgUrl"
+      class="image-size"
+      :alt="props.op.name"
+    />
   </div>
+</RouterLink>
 </template>
 
 <style scoped>
