fef26b97455df086c4ce97dcefb8601ce5fbfc79
nathandinh <nathan.dinh03@outlook.com>
2024-01-13 23:50:25 -0500

    Changed the color on prop container

--- src/components/api_documentation/PropWithoutCode.vue
+++ src/components/api_documentation/PropWithoutCode.vue
@@ -23,7 +23,7 @@ const props = defineProps(["item"]);
 }
 .prop-container div h4 {
   padding: 0.2em 0.5em 0.2em 0.5em;
-  background: rgb(142, 156, 165);
+  background: rgb(57, 81, 158);
   border-radius: 5px;
   margin-right: 0.5em;
 }
